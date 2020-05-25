c196e5731f2ffa62d719cc79b6708d197aac0a0b
Marshall Brain <marshalldbrain@gmail.com>
2020-05-25 16:41:00 -0400

    Setup gradle
    Initializes a multi-platform kotlin project.

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("multiplatform") version "1.3.72"
+}
+
+group = "com.marshalldbrain.pulsar"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+
+    jvm()
+
+    sourceSets {
+
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        jvm().compilations["main"].defaultSourceSet {
+            dependencies {
+                implementation(kotlin("stdlib-jdk7"))
+                implementation("org.slf4j:slf4j-api:1.7.5")
+                implementation("org.slf4j:slf4j-log4j12:1.7.5")
+            }
+        }
+
+        jvm().compilations["test"].defaultSourceSet {
+            dependencies {
+                implementation("io.kotlintest:kotlintest-runner-junit5:3.3.0")
+                implementation("org.assertj:assertj-core:3.11.1")
+                implementation("io.mockk:mockk:1.9.3")
+            }
+        }
+
+    }
+}
