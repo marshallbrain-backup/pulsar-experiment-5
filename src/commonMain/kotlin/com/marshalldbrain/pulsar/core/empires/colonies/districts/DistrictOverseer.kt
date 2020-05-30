ecb4ee4092ef72c4cb6ffb625494f2bd00801748
Marshall Brain <marshalldbrain@gmail.com>
2020-05-29 22:14:38 -0400

    Add build order checking and creation

--- src/commonMain/kotlin/com/marshalldbrain/pulsar/core/empires/colonies/districts/DistrictOverseer.kt
+++ src/commonMain/kotlin/com/marshalldbrain/pulsar/core/empires/colonies/districts/DistrictOverseer.kt
@@ -1,10 +1,21 @@
 package com.marshalldbrain.pulsar.core.empires.colonies.districts
 
+import com.marshalldbrain.pulsar.core.empires.colonies.construction.BuildTask
+import com.marshalldbrain.pulsar.core.empires.colonies.construction.BuildTaskImpl
+import com.marshalldbrain.pulsar.core.empires.colonies.construction.BuildType
+
 class DistrictOverseer (private val DistrictTypes: Set<DistrictType>) {
 
     private val max = 25
     private val maxSlots = 4
     private val allocation = mutableMapOf<DistrictType, Int>()
+    private val notImplementedMessage = "has not been implemented as possible construction types for " +
+            "districts but plans to be implemented in the future."
+
+    val allocated: Int
+        get() = allocation.values.sum()
+    val allocatedSlots: Int
+        get() = allocation.size
 
     init {
 
@@ -17,9 +28,41 @@ class DistrictOverseer (private val DistrictTypes: Set<DistrictType>) {
 
     }
 
-    private fun getPossibleDistricts(): List<DistrictType> =
-        DistrictTypes.filter {
-            it.possible()
+    private fun getPossibleDistricts(): List<DistrictType> = DistrictTypes.filter { it.possible() }
+
+    internal fun check(target: DistrictType, type: BuildType, amount: Int): Boolean {
+
+        if (target in getPossibleDistricts()) {
+            return when(type) {
+                BuildType.BUILD -> {
+                    target in allocation && allocated + amount <= max
+                }
+                BuildType.DESTROY, BuildType.RETOOL, BuildType.UPGRADE -> {
+                    throw NotImplementedError("$type $notImplementedMessage")
+                }
+                else -> false
+            }
         }
+
+        return false
+
+    }
+
+    internal fun createOrder(target: DistrictType, type: BuildType, amount: Int): BuildTaskImpl {
+
+        return when(type) {
+            BuildType.BUILD -> {
+                BuildTaskImpl(target, type, target.buildTime, target.cost, amount) {
+                    allocation[target] = allocation.getValue(target) + 1
+                }
+            }
+            BuildType.DESTROY, BuildType.RETOOL, BuildType.UPGRADE -> {
+                throw NotImplementedError("$type $notImplementedMessage")
+            }
+            else -> throw UnsupportedOperationException("$type is not a supported build type for districts. " +
+                    "This should also never be seen and is a bug if it is")
+        }
+
+    }
 
 }
