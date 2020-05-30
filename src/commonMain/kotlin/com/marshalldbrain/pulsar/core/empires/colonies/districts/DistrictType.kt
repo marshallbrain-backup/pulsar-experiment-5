53ad8664e2c7c21f193245b119f743fdd3e6e956
Marshall Brain <marshalldbrain@gmail.com>
2020-05-29 22:15:36 -0400

    Implement the Buildable interface

--- src/commonMain/kotlin/com/marshalldbrain/pulsar/core/empires/colonies/districts/DistrictType.kt
+++ src/commonMain/kotlin/com/marshalldbrain/pulsar/core/empires/colonies/districts/DistrictType.kt
@@ -1,15 +1,17 @@
 package com.marshalldbrain.pulsar.core.empires.colonies.districts
 
+import com.marshalldbrain.pulsar.core.empires.colonies.construction.Buildable
 import com.marshalldbrain.pulsar.core.resources.ResourceType
 
 data class DistrictType (
     val id: String,
+    val buildTime: Int,
     val cost: Map<ResourceType, Int>,
     val production: Map<ResourceType, Int>,
     val upkeep: Map<ResourceType, Int>,
     private val possible: () -> Boolean,
     private val starting: () -> Boolean
-) {
+) : Buildable {
 
     fun possible(): Boolean {
         return possible.invoke()
