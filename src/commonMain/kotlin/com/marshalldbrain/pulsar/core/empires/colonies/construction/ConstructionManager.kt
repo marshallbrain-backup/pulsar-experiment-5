9e4306f605ed13a7a171165c1c7cb5eee17cd7f0
Marshall Brain <marshalldbrain@gmail.com>
2020-05-31 16:45:06 -0400

    Implement function to processes build time

--- src/commonMain/kotlin/com/marshalldbrain/pulsar/core/empires/colonies/construction/ConstructionManager.kt
+++ src/commonMain/kotlin/com/marshalldbrain/pulsar/core/empires/colonies/construction/ConstructionManager.kt
@@ -1,6 +1,6 @@
 package com.marshalldbrain.pulsar.core.empires.colonies.construction
 
-import com.marshalldbrain.ion.collections.Queue
+import com.marshalldbrain.ion.ImpossibleStateException
 import com.marshalldbrain.ion.collections.queueOf
 
 class ConstructionManager {
@@ -18,6 +18,27 @@ class ConstructionManager {
         } else {
             queue.push(task)
         }
+    }
+
+    fun processTime(timePassed: Int) {
+        var buildRemaining = timePassed
+
+        while (buildRemaining > 0) {
+            buildRemaining = tasks[0].build(buildRemaining)
+            if (buildRemaining >= 0 && tasks[0].isDone) {
+                tasks.removeAt(0)
+                if (tasks.isNotEmpty()) {
+                    throw ImpossibleStateException()
+                }
+                if (queue.isNotEmpty()) {
+                    tasks.add(queue.pop())
+                }
+            } else if ((buildRemaining < 0 && tasks[0].isDone) || (buildRemaining >= 0 && !tasks[0].isDone)) {
+                throw ImpossibleStateException()
+            }
+
+        }
+
     }
 
 }
