d2584643b68a05582d3db963fd592949ebd64396
Marshall Brain <marshalldbrain@gmail.com>
2020-11-01 23:12:48 -0500

    Update dependencies and gradle logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    kotlin("multiplatform") version "1.3.72"
+    id("com.github.ben-manes.versions") version "0.33.0"
+    kotlin("multiplatform") version "1.4.20-M1"
 }
 
 group = "com.marshalldbrain"
@@ -7,11 +10,10 @@ version = "0.1"
 
 repositories {
     mavenCentral()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 kotlin {
-
-    jvm()
 
     sourceSets {
 
@@ -24,28 +26,34 @@ kotlin {
 
         jvm().compilations["main"].defaultSourceSet {
             dependencies {
+                dependsOn(commonMain)
                 implementation(kotlin("stdlib-jdk7"))
-                implementation("org.slf4j:slf4j-api:1.7.5")
-                implementation("org.slf4j:slf4j-log4j12:1.7.5")
+                implementation("org.slf4j:slf4j-api:2.0.0-alpha1")
+                implementation("org.slf4j:slf4j-log4j12:2.0.0-alpha1")
             }
         }
 
         jvm().compilations["test"].defaultSourceSet {
             dependencies {
-                implementation("io.kotest:kotest-runner-junit5-jvm:4.0.5") // for kotest framework
-                implementation("io.kotest:kotest-assertions-core-jvm:4.0.5") // for kotest core jvm assertions
-                implementation("io.kotest:kotest-property-jvm:4.0.5") // for kotest property test
-                implementation("org.assertj:assertj-core:3.11.1")
-                implementation("io.mockk:mockk:1.9.3")
+                dependsOn(commonMain)
+                implementation(kotlin("stdlib-jdk8"))
+                implementation("io.kotest:kotest-runner-junit5-jvm:4.3.1")
+                implementation("io.kotest:kotest-assertions-core-jvm:4.3.1")
+                implementation("io.kotest:kotest-property-jvm:4.3.1")
+                implementation("org.assertj:assertj-core:3.18.0")
+                implementation("io.mockk:mockk:1.10.2")
             }
-        }
 
-        tasks.withType<Test> {
-            useJUnitPlatform()
+            tasks.withType<Test> {
+                useJUnitPlatform()
+                testLogging {
+                    outputs.upToDateWhen {false}
+                    showStandardStreams = true
+                }
+            }
+
         }
 
     }
 
 }
-
-apply("dependencies.gradle.kts")
